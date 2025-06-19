77a548d63f11715eb6d0490a2bbbdaae964e71d7
Failfer <dceaton9@gmail.com>
2025-06-18 21:07:33 -0700

    added basic js fully

--- muscles.js
+++ muscles.js
@@ -2,25 +2,30 @@ let annualRain = 0;
 let sqrFootage = 0;
 let conversionRatio = 0.62;
 
+let galons = 0;
 let rainInput = document.getElementById("rainfall");
 let feetInput = document.getElementById("sqrfeet");
 let resultsText = document.getElementById("result");
 
 
-rainInput.addEventListener('input', updateResults);
-feetInput.addEventListener('input', getAnnualRain);
+rainInput.addEventListener("input", getAnnualRain);
+feetInput.addEventListener("input", getSquareFeet);
 function getSquareFeet(){
-    sqrFootage = feetInput.value;
+    sqrFootage = parseFloat(feetInput.value);
     updateResults();
 
 }
 function getAnnualRain(){
-    annualRain = rainInput.value;
+    annualRain = parseFloat(rainInput.value);
     updateResults();
 
 }
 
 function updateResults(){
-    resultsText.textContent = "HAHAHAHHAHHAHAH"
+    if(annualRain != 0 & sqrFootage != 0){
+        galons = sqrFootage * annualRain * conversionRatio;
+        resultsText.textContent = galons + " galons of water per year";
+    }
+    
 
 }
